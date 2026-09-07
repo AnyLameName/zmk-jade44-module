e72de56847317080eda7c14cf380ca74fd63af72
Jay Gagnon <jay.gagnon@gmail.com>
2026-09-06 23:30:24 -0400

    Some space/footprint optimizations:
    * Screw hole adjustments to make them a bit safer in terms of keyswitch collisions.
    * Slight increase to padding on the horizontal edges, as the switch socket pads were a bit squeezed.
    * Eliminate through-hole diode options to clean up the visuals on the front of the board as well as free up a surprising amount of space.
    * Move reset switch up next to power switch.
    * Move battery connector to middle of board.

--- ergogen/watch.sh
+++ ergogen/watch.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "config.yaml" | entr -c -s "date && echo '' && ergogen . --svg"
+echo "config.yaml" | entr -c -s "date && echo '' && ergogen . --svg && cp ./output/pcbs/jade44.kicad_pcb ~/src/jade44-pcb/jade44.kicad_pcb"
